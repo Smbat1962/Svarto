61dd5471465c9bffe376fb35024d4dc5d075e4dc
milaaspram <parsadanyansmbat@gmail.com>
2023-09-27 00:06:41 +0400

    Added any string metod

--- corrected_my_str.py
+++ corrected_my_str.py
@@ -30,14 +30,59 @@ class My_Str:
             else:
                 s1 += i
         return s1
+    
+    def isalnum(self):
+        s1 = "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpRrSsTtUuVvWwXxYyZz"
+        for simvol in self.s:
+            if simvol not in s1:
+                print("False")
+                break            
+        else:
+            print("True")
+        
+    def isalpha(self):
+        s1 = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpRrSsTtUuVvWwXxYyZz"
+        for simvol in self.s:
+            if simvol not in s1:
+                print("False")
+                break            
+        else:
+            print("True")
+
+    def isdigit(self):
+        s1 = "0123456789"
+        for simvol in self.s:
+            if simvol not in s1:
+                print("False")
+                break            
+        else:
+            print("True")
+
+    def isidentifier(self):
+        s1 = "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpRrSsTtUuVvWwXxYyZz_"
+        s2 = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpRrSsTtUuVvWwXxYyZz_"
+        for simvol in self.s:
+            if self.s[0] not in s2:
+                print("False")
+                break            
+            elif simvol not in s1:
+                print("False")
+                break
+        else:
+            print("True")
 
            
 def main():
 
     my_str = My_Str("Hello, Hello I am {} And my age is {} Welcome tocom My id is {} comWor\tld W\torld")
+    #my_str = My_Str("D584lkj_69")
     print(my_str.count("llo",1,15))
     print(my_str.endswith("llo",1,5))
     print(my_str.format("klon","45","f5"))
+    my_str.isalnum()
+    my_str.isalpha()
+    my_str.isdigit()
+    my_str.isidentifier()
 
 if __name__ == "__main__":
     main()
